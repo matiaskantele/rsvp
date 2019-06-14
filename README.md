a0660934f24d6bb1e67f9e3a0c216a072302aa44
matias.kantele <matias.kantele@siili.com>
2019-06-14 23:56:20 +0300

    I love you!

---
+++ README.md
@@ -0,0 +1,26 @@
+# RSVP
+
+Website for our wedding where people can see information and RSVP.
+
+## Get started
+
+Install the dependencies...
+
+```bash
+cd svelte-app
+npm install
+```
+
+...then start [Rollup](https://rollupjs.org):
+
+```bash
+npm run dev
+```
+
+Navigate to [localhost:5000](http://localhost:5000). You should see your app running. Edit a component file in `src`, save it, and reload the page to see your changes.
+
+## Deploying to the web
+
+### With Netlify
+
+TODO
---
+++ src/App.svelte
@@ -0,0 +1,38 @@
+<style>
+  main {
+    font-family: "Montserrat", sans-serif;
+    height: 100vh;
+    width: 100vw;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    text-align: center;
+    background-color: hsl(0, 79%, 66%);
+  }
+
+  h1 {
+    font-size: 2rem;
+    font-weight: 600;
+    margin: 0;
+    color: hsl(0, 0%, 100%);
+    text-shadow: 5px 5px 0 rgba(0, 0, 0, 0.7);
+    transform: skew(0, -6.7deg);
+    transition-property: font-size;
+    transition-duration: 0.5s;
+  }
+
+  main > h1:first-child {
+    margin-bottom: 1rem;
+  }
+
+  @media (min-width: 920px) {
+    h1 {
+      font-size: 3rem;
+    }
+  }
+</style>
+
+<main>
+  <h1>M ❤️ V</h1>
+  <h1>Summer 2020</h1>
+</main>
