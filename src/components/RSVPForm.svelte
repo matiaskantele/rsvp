bffef574a3231cc3994601e6b79a264c39c61708
Matias Kantele <matias.kantele@siili.com>
2019-07-13 21:59:25 +0300

    testing netlify forms

--- src/components/RSVPForm.svelte
+++ src/components/RSVPForm.svelte
@@ -1,9 +1,5 @@
 <script>
   let attending = "no";
-
-  function submitForm() {
-    console.log(attending);
-  }
 </script>
 
 <style>
@@ -14,15 +10,10 @@
   }
 </style>
 
-<form
-  name="rsvp"
-  method="POST"
-  data-netlify="true"
-  action="/thank-you"
-  on:submit|preventDefault>
+<form name="rsvp" method="POST" data-netlify="true" action="/thank-you">
   <label for="negative">No</label>
   <input type="radio" bind:group={attending} value="no" id="negative" />
   <label for="affirmative">Yes</label>
   <input type="radio" bind:group={attending} value="yes" id="affirmative" />
-  <button type="submit" on:click={submitForm}>RSVP</button>
+  <button type="submit">RSVP</button>
 </form>
