0befbe93d149fc71e1167e626e728129e70c7930
matias.kantele <matias.kantele@siili.com>
2019-11-25 08:43:45 +0200

    apply file naming case change on git

---
+++ components/Nav.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Link from "next/link";
+
+import NavStyles from "./styles/NavStyles";
+import LanguageSelect from "./LanguageSelect";
+
+const Nav = () => (
+  <NavStyles>
+    <LanguageSelect />
+    <Link href="/">
+      <a>Invite</a>
+    </Link>
+    <Link href="info">
+      <a>Info</a>
+    </Link>
+    <Link href="rsvp">
+      <a>RSVP</a>
+    </Link>
+  </NavStyles>
+);
+
+export default Nav;
