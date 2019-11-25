e6d5987675d77b5c9c42f9517e1206570114aeb9
Matias Kantele <matias.kantele@siili.com>
2019-11-25 10:22:06 +0200

    move styled components config to app

--- components/Page.js
+++ components/Page.js
@@ -2,16 +2,6 @@ import Meta from "./Meta";
 import Header from "./Header";
 import styled, { ThemeProvider, createGlobalStyle } from "styled-components";
 
-const theme = {
-  red: "#FF0000",
-  black: "#393939",
-  grey: "#3A3A3A",
-  lightgrey: "#E1E1E1",
-  offWhite: "#EDEDED",
-  maxWidth: "1000px",
-  bs: "0 12px 24px 0 hsla(0, 0%, 0%, 0.09)"
-};
-
 const StyledPage = styled.div`
   background: white;
   color: ${props => props.theme.black};
@@ -23,42 +13,12 @@ const Inner = styled.div`
   padding: 2rem;
 `;
 
-const GlobalStyle = createGlobalStyle`
-  @font-face {
-    font-family: 'radnika_next';
-    src: url('radnikanext-medium-webfont.woff2') format('woff2');
-    font-weight: normal;
-    font-style: normal;
-  }
-  html {
-    box-sizing: border-box;
-    font-size: 10px;
-  }
-  *, *:before, *:after {
-    box-sizing: inherit;
-  }
-  body {
-    padding: 0;
-    margin: 0;
-    font-size: 1.5rem;
-    line-height: 2;
-    font-family: 'radnika_next';
-  }
-  a {
-    text-decoration: none;
-    color: ${props => props.theme.black};
-  }
-`;
-
 const Page = props => (
-  <ThemeProvider theme={theme}>
-    <StyledPage>
-      <GlobalStyle />
-      <Meta />
-      <Header />
-      <Inner>{props.children}</Inner>
-    </StyledPage>
-  </ThemeProvider>
+  <StyledPage>
+    <Meta />
+    <Header />
+    <Inner>{props.children}</Inner>
+  </StyledPage>
 );
 
 export default Page;
--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import App from "next/app";
+import { ThemeProvider, createGlobalStyle } from "styled-components";
 import "../common/i18n";
 
 import Page from "../components/Page";
@@ -6,10 +7,51 @@ import Page from "../components/Page";
 class AppInit extends App {
   render() {
     const { Component, pageProps } = this.props;
+
+    const theme = {
+      red: "#FF0000",
+      black: "#393939",
+      grey: "#3A3A3A",
+      lightgrey: "#E1E1E1",
+      offWhite: "#EDEDED",
+      maxWidth: "1000px",
+      bs: "0 12px 24px 0 hsla(0, 0%, 0%, 0.09)"
+    };
+
+    const GlobalStyle = createGlobalStyle`
+    @font-face {
+      font-family: 'radnika_next';
+      src: url('radnikanext-medium-webfont.woff2') format('woff2');
+      font-weight: normal;
+      font-style: normal;
+    }
+    html {
+      box-sizing: border-box;
+      font-size: 10px;
+    }
+    *, *:before, *:after {
+      box-sizing: inherit;
+    }
+    body {
+      padding: 0;
+      margin: 0;
+      font-size: 1.5rem;
+      line-height: 2;
+      font-family: 'radnika_next';
+    }
+    a {
+      text-decoration: none;
+      color: ${props => props.theme.black};
+    }
+  `;
+
     return (
-      <Page>
-        <Component {...pageProps} />
-      </Page>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <Page>
+          <Component {...pageProps} />
+        </Page>
+      </ThemeProvider>
     );
   }
 }
