c7c6507fadf7299676e29fa628a584936305c767
Matias Kantele <matias.kantele@siili.com>
2019-12-28 13:51:25 +0200

    return of netlify form submission difficulties

--- components/Form.js
+++ components/Form.js
@@ -37,7 +37,7 @@ const hiddenInputsForNetlifyForms = (
       "songs",
       "message",
     ].map(i => (
-      <input name={i} key={i} />
+      <input type="text" name={i} key={i} />
     ))}
   </Hidden>
 );
