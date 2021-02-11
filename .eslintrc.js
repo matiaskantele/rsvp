dc7f1f3d9db1f30249d56cb18b661959dee8606d
Matias Kantele <matias.kantele@gmail.com>
2021-02-11 13:29:13 +0200

    add description and images to readme

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,142 +1,125 @@
 module.exports = {
-  parser: "@typescript-eslint/parser",
+  parser: '@typescript-eslint/parser',
   extends: [
-    "airbnb",
-    "eslint:recommended",
-    "prettier/@typescript-eslint",
-    "plugin:react/recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:prettier/recommended"
+    'airbnb',
+    'eslint:recommended',
+    'prettier/@typescript-eslint',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
   ],
-  plugins: [
-    "html"
-  ],
+  plugins: ['html'],
   parserOptions: {
     ecmaVersion: 2020,
-    sourceType: "module",
+    sourceType: 'module',
     ecmaFeatures: {
       impliedStrict: true,
       classes: true,
-      jsx: true
-    }
+      jsx: true,
+    },
   },
   env: {
     browser: true,
     node: true,
     jquery: true,
-    jest: true
+    jest: true,
   },
   settings: {
-    'import/extensions': [".js",".jsx",".ts",".tsx"],
+    'import/extensions': ['.js', '.jsx', '.ts', '.tsx'],
     'import/parsers': {
-      '@typescript-eslint/parser': [".ts",".tsx"]
+      '@typescript-eslint/parser': ['.ts', '.tsx'],
+    },
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
       },
-    "import/resolver": {
-      "node": {
-        "extensions": [".js", ".jsx", ".ts", ".tsx"]
-      }
-    }
+    },
   },
   rules: {
-    "react/jsx-filename-extension": [2, { extensions: [".js", ".jsx", ".ts", ".tsx"] }],
-    "react/react-in-jsx-scope": "off",
-    "max-classes-per-file": [
-      "error",
-      10
-    ],
-    "@typescript-eslint/no-non-null-assertion": 0,
-    "no-debugger": 0,
-    "no-alert": 0,
-    "no-await-in-loop": 0,
-    "no-return-assign": [
-      "error",
-      "except-parens"
-    ],
-    "no-restricted-syntax": [
+    'react/jsx-filename-extension': [
       2,
-      "ForInStatement",
-      "LabeledStatement",
-      "WithStatement"
+      { extensions: ['.js', '.jsx', '.ts', '.tsx'] },
     ],
-    "no-unused-vars": [
+    'react/react-in-jsx-scope': 'off',
+    'max-classes-per-file': ['error', 10],
+    '@typescript-eslint/no-non-null-assertion': 0,
+    'no-debugger': 0,
+    'no-alert': 0,
+    'no-await-in-loop': 0,
+    'no-return-assign': ['error', 'except-parens'],
+    'no-restricted-syntax': [
+      2,
+      'ForInStatement',
+      'LabeledStatement',
+      'WithStatement',
+    ],
+    'no-unused-vars': [
       1,
       {
-        "ignoreSiblings": true,
-        "argsIgnorePattern": "res|next|^err"
-      }
+        ignoreSiblings: true,
+        argsIgnorePattern: 'res|next|^err',
+      },
     ],
-    "prefer-const": [
-      "error",
+    'prefer-const': [
+      'error',
       {
-        "destructuring": "all"
-      }
+        destructuring: 'all',
+      },
     ],
-    "arrow-body-style": [
-      2,
-      "as-needed"
-    ],
-    "no-unused-expressions": [
+    'arrow-body-style': [2, 'as-needed'],
+    'no-unused-expressions': [
       2,
       {
-        "allowTaggedTemplates": true
-      }
+        allowTaggedTemplates: true,
+      },
     ],
-    "no-param-reassign": [
+    'no-param-reassign': [
       2,
       {
-        "props": false
-      }
+        props: false,
+      },
     ],
-    "no-console": 0,
-    "import/prefer-default-export": 0,
-    "import": 0,
-    "func-names": 0,
-    "space-before-function-paren": 0,
-    "comma-dangle": 0,
-    "max-len": 0,
-    "import/extensions": 0,
-    "no-underscore-dangle": 0,
-    "consistent-return": 0,
-    "jsx-a11y/accessible-emoji": 0,
-    "radix": 0,
-    "no-shadow": [
+    'no-console': 0,
+    'import/prefer-default-export': 0,
+    import: 0,
+    'func-names': 0,
+    'space-before-function-paren': 0,
+    'comma-dangle': 0,
+    'max-len': 0,
+    'import/extensions': 0,
+    'no-underscore-dangle': 0,
+    'consistent-return': 0,
+    'jsx-a11y/accessible-emoji': 0,
+    radix: 0,
+    'no-shadow': [
       2,
       {
-        "hoist": "all",
-        "allow": [
-          "resolve",
-          "reject",
-          "done",
-          "next",
-          "err",
-          "error"
-        ]
-      }
+        hoist: 'all',
+        allow: ['resolve', 'reject', 'done', 'next', 'err', 'error'],
+      },
     ],
-    "quotes": [
+    quotes: [
       2,
-      "single",
+      'single',
       {
-        "avoidEscape": true,
-        "allowTemplateLiterals": true
-      }
+        avoidEscape: true,
+        allowTemplateLiterals: true,
+      },
     ],
-    "prettier/prettier": [
-      "error",
+    'prettier/prettier': [
+      'error',
       {
-        "trailingComma": "es5",
-        "singleQuote": true,
-        "printWidth": 80
-      }
+        trailingComma: 'es5',
+        singleQuote: true,
+        printWidth: 80,
+      },
     ],
-    "jsx-a11y/href-no-hash": "off",
-    "jsx-a11y/anchor-is-valid": [
-      "warn",
+    'jsx-a11y/href-no-hash': 'off',
+    'jsx-a11y/anchor-is-valid': [
+      'warn',
       {
-        "aspects": [
-          "invalidHref"
-        ]
-      }
-    ]
-  }
-}
+        aspects: ['invalidHref'],
+      },
+    ],
+  },
+};
--- README.md
+++ README.md
@@ -2,52 +2,20 @@
 
 [![Netlify Status](https://api.netlify.com/api/v1/badges/f9b129e6-a4a6-493d-8fcd-9bf90324c41d/deploy-status)](https://app.netlify.com/sites/our-wedding/deploys)
 
-Website for our wedding where people can see information and RSVP.
+Information and RSVP form for our wedding guests.
 
-## Get started
+Built with [TypeScript](https://www.typescriptlang.org/), [React](https://reactjs.org/) and [Next.js](https://nextjs.org/). Exported as a static site and hosted on [Netlify](https://www.netlify.com/).
 
-Install the dependencies...
+## Pages
 
-```bash
-cd rsvp
-npm install
-```
+### Home page
 
-...then start [Rollup](https://rollupjs.org):
+![Image of home page](/public/home.jpg)
 
-```bash
-npm run dev
-```
+### Info page
 
-Navigate to [localhost:3000](http://localhost:3000). You should see your app running. Edit a component file in `src`, save it, and reload the page to see your changes.
+![Image of Info page](/public/info.jpg)
 
-Consult [sapper.svelte.dev](https://sapper.svelte.dev) for help getting started.
+### RSVP form
 
-## Structure
-
-Sapper expects to find two directories in the root of your project — `src` and `static`.
-
-### src
-
-The [src](src) directory contains the entry points for your app — `client.js`, `server.js` and (optionally) a `service-worker.js` — along with a `template.html` file and a `routes` directory.
-
-#### src/routes
-
-This is the heart of your Sapper app. There are two kinds of routes — _pages_, and _server routes_.
-
-**Pages** are Svelte components written in `.svelte` files. When a user first visits the application, they will be served a server-rendered version of the route in question, plus some JavaScript that 'hydrates' the page and initialises a client-side router. From that point forward, navigating to other pages is handled entirely on the client for a fast, app-like feel. (Sapper will preload and cache the code for these subsequent pages, so that navigation is instantaneous.)
-
-**Server routes** are modules written in `.js` files, that export functions corresponding to HTTP methods. Each function receives Express `request` and `response` objects as arguments, plus a `next` function. This is useful for creating a JSON API, for example.
-
-There are three simple rules for naming the files that define your routes:
-
-- A file called `src/routes/about.svelte` corresponds to the `/about` route. A file called `src/routes/blog/[slug].svelte` corresponds to the `/blog/:slug` route, in which case `params.slug` is available to the route
-- The file `src/routes/index.svelte` (or `src/routes/index.js`) corresponds to the root of your app. `src/routes/about/index.svelte` is treated the same as `src/routes/about.svelte`.
-- Files and directories with a leading underscore do _not_ create routes. This allows you to colocate helper modules and components with the routes that depend on them — for example you could have a file called `src/routes/_helpers/datetime.js` and it would _not_ create a `/_helpers/datetime` route
-
-## Deploying to the web
-
-### With Netlify
-
-It's automatic!
-Deployed automatically after commited to master branch.
+![Image of RSVP form](/public/rsvp.jpg)
